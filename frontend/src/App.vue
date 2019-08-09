eb12bd9d7f62b82c31b1c0411788aa03e3e7cce3
Tom Marienfeld <marienfeldtom@googlemail.com>
2019-08-09 16:07:32 +0200

    seperating logic and .vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,157 +1,100 @@
 <template>
-<div style="height: 100%;">
-<div>
-  <b-nav id="nav">
-  <img src="./assets/logo.png" style="height: 80px; width: auto; margin-top: 10px; margin-left: 30px;">
-    <b-nav-item active><i class="fas fa-list-ul"></i>Einkaufsliste</b-nav-item>
-    <b-nav-item><i class="fas fa-map-pin"></i>Edeka Finder</b-nav-item>
-    
-  <img id="logo" class="" src="https://www.edeka.de/b2c-design/global/core/assets/organisms/m401-header/logo.png">
-  </b-nav>
-</div>
+  <div style="height: 100%;">
+    <div>
+      <b-nav id="nav">
+        <img
+          src="./assets/logo.png"
+          style="height: 80px; width: auto; margin-top: 10px; margin-left: 30px;"
+        />
+        <b-nav-item active>
+          <i class="fas fa-list-ul"></i>Einkaufsliste
+        </b-nav-item>
+        <b-nav-item>
+          <i class="fas fa-map-pin"></i>Edeka Finder
+        </b-nav-item>
 
- <b-container id="cont">
-        <h1>Einkaufsliste:</h1>
-         <b-form-input v-model="data" @keyup="sendMessage()" placeholder="Produkt hinzufügen..."></b-form-input>
+        <img
+          id="logo"
+          class
+          src="https://www.edeka.de/b2c-design/global/core/assets/organisms/m401-header/logo.png"
+        />
+      </b-nav>
+    </div>
 
-<div class="grid">
-  <div v-for="result in results" class="item">
-  <img class="img-fluid" :src="result.doc.bild">
-  <span> {{getNumber()}} {{result.doc.preinheit}} {{getName(result.doc.produktname, result.doc.plural)}}</span>
-  </div>
-</div>
+    <b-container id="cont">
+      <h1>Einkaufsliste:</h1>
+      <b-form-input v-model="data" @keyup="sendMessage()" placeholder="Produkt hinzufügen..."></b-form-input>
 
-        <hr>
-<div class="grid">
-  <div v-for="result in liste" class="item">
-  <img class="img-fluid" :src="result.bild">
-  <span> 1 {{result.preinheit}} {{result.produktname}}</span>
-  </div>
-</div>
-      </b-container>
-
-      <footer>
-      <span>INEZ Code Competion submission by Tom Marienfeld</span>
-      </footer>
+      <div class="grid">
+        <div v-for="result in results" class="item">
+          <img class="img-fluid" :src="result.doc.bild" />
+          <span>{{getNumber()}} {{result.doc.preinheit}} {{getName(result.doc.produktname, result.doc.plural)}}</span>
+        </div>
       </div>
+
+      <hr />
+      <div class="grid">
+        <div v-for="result in liste" class="item">
+          <img class="img-fluid" :src="result.bild" />
+          <span>1 {{result.preinheit}} {{result.produktname}}</span>
+        </div>
+      </div>
+    </b-container>
+
+    <footer>
+      <span>INEZ Code Competion submission by Tom Marienfeld</span>
+    </footer>
+  </div>
 </template>
 
 
 
 <script>
-import io from 'socket.io-client';
+import io from "socket.io-client";
+import { liste } from "./storage.js";
 export default {
-  name: 'app',
+  name: "app",
   data() {
     return {
-      data: '',
+      data: "",
       results: [],
-      liste: [
-    {
-      "produktname": "Karotte",
-      "plural": "Karotten",
-      "bild": "https://www.lebensbaum.com/files/styles/greige_image_476x476/public/images/greiges/karotte1.jpg?itok=ebABzEHu",
-      "kategorie": "Obst & Gemüse",
-      "synonyme": [
-        "Möhre",
-        "Rübe"
-      ],
-      "preinheit": "Stück",
-      "einheiten": [
-        "Packung",
-        "Kilogramm"
-      ]
-    },
-    {
-      "produktname": "Aubergine",
-      "plural": "Auberginen",
-      "bild": "https://res.cloudinary.com/lush/image/upload/v1/lush_content/ingredients/main/2019/01/web_aubergine_ingreedients_2019-2.jpg?itok=YynZ56eL",
-      "kategorie": "Obst & Gemüse",
-      "synonyme": [
-        "Melanzani"
-      ],
-      "preinheit": "Stück",
-      "einheiten": [
-        ""
-      ]
-    },
-    {
-      "produktname": "Banane",
-      "plural": "Bananen",
-      "bild": "https://www.hofladen-sauerland.de/media/image/b0/37/70/Banane_600x600.jpg",
-      "kategorie": "Obst & Gemüse",
-      "synonyme": [
-        ""
-      ],
-      "preinheit": "Stück",
-      "einheiten": [
-        "Bund"
-      ]
-    },
-    {
-      "produktname": "Brot",
-      "plural": "Brote",
-      "bild": "https://www.honold.ch/components/com_mijoshop/opencart/image/cache/catalog/boulangerie/chnusper-dinkel-brot-500x500.png",
-      "kategorie": "Backwaren",
-      "synonyme": [
-        ""
-      ],
-      "preinheit": "Stück",
-      "einheiten": [
-        ""
-      ]
-    },
-    {
-      "produktname": "Zwiebel",
-      "plural": "Zwiebeln",
-      "bild": "https://www.gesundheute.com/wp-content/uploads/2014/10/Onion_on_White-compressed.jpg",
-      "kategorie": "Obst & Gemüse",
-      "synonyme": [
-        ""
-      ],
-      "preinheit": "Stück",
-      "einheiten": [
-        "Beutel"
-      ]
-    }],
-      socket: io(process.env.VUE_APP_ADRESS + ':' + process.env.VUE_APP_PORT)
-    }
+      liste: liste,
+      socket: io(process.env.VUE_APP_ADRESS + ":" + process.env.VUE_APP_PORT)
+    };
   },
   mounted() {
-        this.socket.on('result', (data) => {
-          console.log(data);
-            this.results = data;
-        });
-    },
+    this.socket.on("result", data => {
+      this.results = data;
+    });
+  },
   methods: {
-    sendMessage: function(){
-      if(this.data == "") {
+    sendMessage: function() {
+      if (this.data == "") {
         this.results = [];
       } else {
-      this.socket.emit('query', this.lastWord(this.data));
+        this.socket.emit("query", this.lastWord(this.data));
       }
     },
-   lastWord: function (words) {
-    var n = words.split(" ");
-    return n[n.length - 1];
-},
-  getNumber: function(){
-    var num =  this.data.replace(/[^0-9]/g,'');
-    if(num =="") {
-      num = "1";
-    } 
-    return num;
-
-  },
-  getName: function(produktname, plural) {
-    if(this.getNumber()>1) {
-      return plural;
-    } else {
-      return produktname;
+    lastWord: function(words) {
+      var n = words.split(" ");
+      return n[n.length - 1];
+    },
+    getNumber: function() {
+      var num = this.data.replace(/[^0-9]/g, "");
+      if (num == "") {
+        num = "1";
+      }
+      return num;
+    },
+    getName: function(produktname, plural) {
+      if (this.getNumber() > 1) {
+        return plural;
+      } else {
+        return produktname;
+      }
     }
   }
-  }
-}
+};
 </script>
 
 <style>
@@ -169,12 +112,12 @@ h1 {
 #nav {
   height: 100px;
   background-image: URL("https://www.edeka.de/b2c-design/global/core/assets/images/background-dark.jpg");
-  background-size: contain; 
+  background-size: contain;
   background-repeat: repeat;
   width: 100%;
 }
 
-#cont{
+#cont {
   heigth: 100%;
 }
 
@@ -182,23 +125,23 @@ h1 {
   margin-top: 1rem;
   width: 52px;
   height: 64px;
-  margin-left:auto; 
-  margin-right:40px;
+  margin-left: auto;
+  margin-right: 40px;
 }
 
 .nav-link {
   padding-top: 2rem !important;
   color: white;
-  font-family: 'Work Sans', sans-serif;
+  font-family: "Work Sans", sans-serif;
   font-size: 1.5rem;
 }
 
 @media (max-width: 768px) {
   .nav-link {
-      font-size: 1rem;
-      padding: 0px !important;
-      padding-left: 10px !important;
-      padding-top: 30px !important;
+    font-size: 1rem;
+    padding: 0px !important;
+    padding-left: 10px !important;
+    padding-top: 30px !important;
   }
 }
 
@@ -214,22 +157,22 @@ i {
 }
 
 .grid {
- display: grid;
+  display: grid;
   grid-gap: 15px;
   grid-template-columns: repeat(auto-fit, 100px);
 }
 
 .item {
   border-radius: 4px;
- border-style: solid;
- border-width: 1px;
- border-color: grey;
- background-color: white;
- height: 100px;
-line-height: 10px;
-margin-top: 20px;
-max-width: 200px;
-padding: 3px;
+  border-style: solid;
+  border-width: 1px;
+  border-color: grey;
+  background-color: white;
+  height: 100px;
+  line-height: 10px;
+  margin-top: 20px;
+  max-width: 200px;
+  padding: 3px;
 }
 
 .item:hover {
@@ -238,19 +181,19 @@ padding: 3px;
 
 .item span {
   font-size: 10px;
-  font-family: 'Work Sans', sans-serif;
+  font-family: "Work Sans", sans-serif;
 }
 
-.item img{
- display: block;
+.item img {
+  display: block;
   margin-left: auto;
   margin-right: auto;
   width: 50%;
- height: 64px;
+  height: 64px;
 }
 
 footer {
-  position:fixed;
+  position: fixed;
   background-color: #f5d033;
   height: 40px;
   width: 100%;
---
+++ frontend/src/storage.js
@@ -0,0 +1,68 @@
+export var liste = [
+    {
+      "produktname": "Karotte",
+      "plural": "Karotten",
+      "bild": "https://www.lebensbaum.com/files/styles/greige_image_476x476/public/images/greiges/karotte1.jpg?itok=ebABzEHu",
+      "kategorie": "Obst & Gemüse",
+      "synonyme": [
+        "Möhre",
+        "Rübe"
+      ],
+      "preinheit": "Stück",
+      "einheiten": [
+        "Packung",
+        "Kilogramm"
+      ]
+    },
+    {
+      "produktname": "Aubergine",
+      "plural": "Auberginen",
+      "bild": "https://res.cloudinary.com/lush/image/upload/v1/lush_content/ingredients/main/2019/01/web_aubergine_ingreedients_2019-2.jpg?itok=YynZ56eL",
+      "kategorie": "Obst & Gemüse",
+      "synonyme": [
+        "Melanzani"
+      ],
+      "preinheit": "Stück",
+      "einheiten": [
+        ""
+      ]
+    },
+    {
+      "produktname": "Banane",
+      "plural": "Bananen",
+      "bild": "https://www.hofladen-sauerland.de/media/image/b0/37/70/Banane_600x600.jpg",
+      "kategorie": "Obst & Gemüse",
+      "synonyme": [
+        ""
+      ],
+      "preinheit": "Stück",
+      "einheiten": [
+        "Bund"
+      ]
+    },
+    {
+      "produktname": "Brot",
+      "plural": "Brote",
+      "bild": "https://www.honold.ch/components/com_mijoshop/opencart/image/cache/catalog/boulangerie/chnusper-dinkel-brot-500x500.png",
+      "kategorie": "Backwaren",
+      "synonyme": [
+        ""
+      ],
+      "preinheit": "Stück",
+      "einheiten": [
+        ""
+      ]
+    },
+    {
+      "produktname": "Zwiebel",
+      "plural": "Zwiebeln",
+      "bild": "https://www.gesundheute.com/wp-content/uploads/2014/10/Onion_on_White-compressed.jpg",
+      "kategorie": "Obst & Gemüse",
+      "synonyme": [
+        ""
+      ],
+      "preinheit": "Stück",
+      "einheiten": [
+        "Beutel"
+      ]
+    }];
